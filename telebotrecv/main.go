01711506988a4b3bf261a355f01c96ffa924e9ba
yuliji <yuliji1010@gmail.com>
2019-07-28 19:11:40 +1000

    telebotrecv

---
+++ telebotrecv/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	botToken := os.Getenv("BOTTOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] chat ID: %d; msg: %s", update.Message.From.UserName, update.Message.Chat.ID, update.Message.Text)
+	}
+}
